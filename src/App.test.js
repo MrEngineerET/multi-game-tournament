8a84a87e3014971d04f26d6bc843bb877d42f7eb
Biruk Berhanu <meetbirukberhanu@gmail.com>
2023-01-11 11:52:05 +0300

    add and configure prettier

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from "@testing-library/react"
+import App from "./App"
 
 test("renders learn react link", () => {
-  render(<App />);
-  const element = screen.getByText(/Genesis/i);
-  expect(element).toBeInTheDocument();
-});
+  render(<App />)
+  const element = screen.getByText(/Genesis/i)
+  expect(element).toBeInTheDocument()
+})
