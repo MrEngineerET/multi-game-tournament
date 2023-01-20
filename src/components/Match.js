48f44dba8a054e7138ee0db9e68a87607108dd4a
Biruk Berhanu <meetbirukberhanu@gmail.com>
2023-01-20 16:41:36 +0300

    refactor BracketGenerator

---
+++ src/components/Match.js
@@ -0,0 +1,77 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { Box, Typography, Divider } from "@mui/material"
+
+const matchStyles = {
+  root: {
+    width: 200,
+    borderRadius: "5px",
+    bgcolor: "#bbb",
+  },
+}
+
+const rowStyles = {
+  row: {
+    bgcolor: ({ palette }) => palette.bracket.background,
+    display: "flex",
+    justifyContent: "space-between",
+    alignItem: "center",
+    color: "white",
+  },
+  score: {
+    width: "2rem",
+    p: 0.5,
+    textAlign: "center",
+    bgcolor: (theme) => theme.palette.bracket.score,
+  },
+  name: {
+    p: 0.5,
+    pl: 2,
+  },
+  borderTop: {
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+  },
+  borderBottom: {
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+  },
+}
+
+export const Match = () => {
+  return (
+    <Box sx={matchStyles.root}>
+      <Row name="Player One" score={5} isTop />
+      <Divider color="white" />
+      <Row name="Player Two" score={2} isWinner />
+    </Box>
+  )
+}
+
+function Row({ name, score, isWinner, isTop }) {
+  return (
+    <Box
+      sx={[rowStyles.row, isTop ? rowStyles.borderTop : rowStyles.borderBottom]}
+    >
+      <Typography sx={rowStyles.name} variant="text">
+        {name}
+      </Typography>
+      <Typography
+        sx={[
+          rowStyles.score,
+          isWinner && { bgcolor: (theme) => theme.palette.primary.main },
+          isTop ? { borderTopRightRadius: 5 } : { borderBottomRightRadius: 5 },
+        ]}
+        variant="text"
+      >
+        {score}
+      </Typography>
+    </Box>
+  )
+}
+Row.propTypes = {
+  name: PropTypes.string,
+  score: PropTypes.number,
+  isWinner: PropTypes.boolean,
+  isTop: PropTypes.boolean,
+}
--- src/pages/BracketGenerator.js
+++ src/pages/BracketGenerator.js
@@ -1,80 +1,36 @@
+/* eslint-disable no-unused-vars */
 import React from "react"
-import { Box, Typography, Divider } from "@mui/material"
-import PropTypes from "prop-types"
+import { Box, Stack, Typography } from "@mui/material"
 
-const styles = {
-  paper: {
-    m: 5,
-    border: 1,
-    height: 400,
-    p: 3,
-  },
-  root: {
-    width: 200,
-    borderRadius: "5px",
-    bgcolor: "#bbb",
-  },
-  row: {
-    bgcolor: ({ palette }) => palette.bracket.background,
-    display: "flex",
-    justifyContent: "space-between",
-    alignItem: "center",
-    color: "white",
-  },
-  score: {
-    width: "2rem",
-    p: 0.5,
-    textAlign: "center",
-    bgcolor: (theme) => theme.palette.bracket.score,
-  },
-  name: {
-    p: 0.5,
-    pl: 2,
-  },
-  borderTop: {
-    borderTopLeftRadius: 5,
-    borderTopRightRadius: 5,
-  },
-  borderBottom: {
-    borderBottomLeftRadius: 5,
-    borderBottomRightRadius: 5,
-  },
-}
+import { Match } from "../components/Match"
+
+const styles = {}
 
 export const BracketGenerator = () => {
   return (
-    <Box sx={styles.paper}>
-      <Box sx={styles.root}>
-        <Row name="Player One" score={5} isTop />
-        <Divider color="white" />
-        <Row name="Player Two" score={2} isWinner />
-      </Box>
+    <Box p={5}>
+      <Stack direction="row" gap={5}>
+        <Stack gap={5}>
+          <Typography>Round One</Typography>
+          <Match />
+          <Match />
+        </Stack>
+        <Stack gap={5}>
+          <Typography>Round Two</Typography>
+          <Match />
+          <Match />
+        </Stack>
+        <Stack gap={5}>
+          <Typography>Round Three</Typography>
+          <Match />
+          <Match />
+        </Stack>
+        <Stack gap={5}>
+          <Typography>Final Round</Typography>
+          <Match />
+          <Match />
+        </Stack>
+      </Stack>
     </Box>
   )
-}
-
-function Row({ name, score, isWinner, isTop }) {
-  return (
-    <Box sx={[styles.row, isTop ? styles.borderTop : styles.borderBottom]}>
-      <Typography sx={styles.name} variant="text">
-        {name}
-      </Typography>
-      <Typography
-        sx={[
-          styles.score,
-          isWinner && { bgcolor: (theme) => theme.palette.primary.main },
-          isTop ? { borderTopRightRadius: 5 } : { borderBottomRightRadius: 5 },
-        ]}
-        variant="text"
-      >
-        {score}
-      </Typography>
-    </Box>
-  )
-}
-Row.propTypes = {
-  name: PropTypes.string,
-  score: PropTypes.number,
-  isWinner: PropTypes.boolean,
-  isTop: PropTypes.boolean,
 }
