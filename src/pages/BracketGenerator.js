92b8cd1e076a01815a83b4e486a20efe8ae44197
Biruk Berhanu <meetbirukberhanu@gmail.com>
2023-01-20 17:11:59 +0300

    create a ui for single elimination on 16 teams

--- src/pages/BracketGenerator.js
+++ src/pages/BracketGenerator.js
@@ -4,31 +4,98 @@ import { Box, Stack, Typography } from "@mui/material"
 
 import { Match } from "../components/Match"
 
-const styles = {}
+const styles = {
+  column: {
+    height: "100%",
+  },
+  row: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  header: {
+    display: "flex",
+    justifyContent: "center",
+  },
+}
 
 export const BracketGenerator = () => {
   return (
     <Box p={5}>
-      <Stack direction="row" gap={5}>
-        <Stack gap={5}>
-          <Typography>Round One</Typography>
-          <Match />
-          <Match />
+      <Stack direction="row" gap={15}>
+        <Stack gap={2}>
+          <Box sx={styles.header}>
+            <Typography variant="h5">Round One</Typography>
+          </Box>
+          <Stack gap={6} sx={styles.column}>
+            <Box sx={styles.row}>
+              <Match />
+            </Box>
+            <Box sx={styles.row}>
+              <Match />
+            </Box>
+            <Box sx={styles.row}>
+              <Match />
+            </Box>
+            <Box sx={styles.row}>
+              <Match />
+            </Box>
+            <Box sx={styles.row}>
+              <Match />
+            </Box>
+            <Box sx={styles.row}>
+              <Match />
+            </Box>
+            <Box sx={styles.row}>
+              <Match />
+            </Box>
+            <Box sx={styles.row}>
+              <Match />
+            </Box>
+          </Stack>
         </Stack>
-        <Stack gap={5}>
-          <Typography>Round Two</Typography>
-          <Match />
-          <Match />
+        <Stack gap={2}>
+          <Box sx={styles.header}>
+            <Typography variant="h5">Round Two</Typography>
+          </Box>
+          <Stack gap={4} sx={styles.column}>
+            <Box sx={styles.row}>
+              <Match />
+            </Box>
+            <Box sx={styles.row}>
+              <Match />
+            </Box>
+            <Box sx={styles.row}>
+              <Match />
+            </Box>
+            <Box sx={styles.row}>
+              <Match />
+            </Box>
+          </Stack>
         </Stack>
-        <Stack gap={5}>
-          <Typography>Round Three</Typography>
-          <Match />
-          <Match />
+        <Stack gap={2}>
+          <Box sx={styles.header}>
+            <Typography variant="h5">Round Three</Typography>
+          </Box>
+          <Stack gap={4} sx={styles.column}>
+            <Box sx={styles.row}>
+              <Match />
+            </Box>
+            <Box sx={styles.row}>
+              <Match />
+            </Box>
+          </Stack>
         </Stack>
-        <Stack gap={5}>
-          <Typography>Final Round</Typography>
-          <Match />
-          <Match />
+        <Stack gap={2}>
+          <Box sx={styles.header}>
+            <Typography variant="h5">Final Round</Typography>
+          </Box>
+          <Stack gap={4} sx={styles.column}>
+            <Box sx={styles.row}>
+              <Match />
+            </Box>
+          </Stack>
         </Stack>
       </Stack>
     </Box>
