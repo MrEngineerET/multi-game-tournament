a26885484eaa7074d795a45c0bb5d16c2df92d4a
Biruk Berhanu <meetbirukberhanu@gmail.com>
2023-01-30 21:19:17 +0300

    add a new function which fetch a tournaments in tournament api

--- src/api/tournament.js
+++ src/api/tournament.js
@@ -1,22 +1,54 @@
 import { InMemoryDatabase } from "brackets-memory-db"
 import { BracketsManager } from "brackets-manager"
+import ls from "../utils/localStorage"
+import { nanoid } from "nanoid"
 
 const storage = new InMemoryDatabase()
 const bracketsManager = new BracketsManager(storage)
 
-export async function createTournament() {
-  // eslint-disable-next-line no-unused-vars
+export const stageType = {
+  singleElimination: "single_elimination",
+  doubleElimination: "double_elimination",
+  round_robin: "round_robin",
+}
+
+export async function createTournament(
+  name,
+  description,
+  participants,
+  stageType,
+) {
+  //create a tournament
   let query = {
-    name: "aa",
-    seeding: ["a", "b", "c", "d"],
+    name,
+    seeding: participants,
     settings: {
       seedOrdering: ["natural"],
       consolationFinal: false,
     },
-    tournamentId: 0,
-    type: "single_elimination",
+    tournamentId: 1,
+    type: stageType,
   }
   const bracket = await bracketsManager.create(query)
+  const newTournament = { ...bracket, ...storage.data, id: nanoid() }
+  //save to local storage
+  let tournaments = ls.getItem("tournaments")
+  if (!tournaments) tournaments = [newTournament]
+  else tournaments.push(newTournament)
 
-  return { ...bracket, ...storage.data }
+  ls.setItem("tournaments", tournaments)
+  //return the tournament
+  return newTournament
+}
+
+export async function getTournaments() {
+  const tournaments = ls.getItem("tournaments")
+  if (!tournaments) return []
+  return tournaments
+}
+export async function getTournament(id) {
+  const tournaments = ls.getItem("tournaments")
+  if (!tournaments) return null
+  const tournament = tournaments.find((tournament) => tournament.id == id)
+  return tournament
 }
