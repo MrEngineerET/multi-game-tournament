286445ad178fa819aa935b6391ee65962f9e49a7
Biruk Berhanu <meetbirukberhanu@gmail.com>
2023-02-04 21:32:47 +0300

    connect the app with mongodb server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,19 @@
+import mongoose from "mongoose"
 import { app } from "./app.js"
 
+import { config } from "dotenv"
+config()
+
 const PORT = process.env.PORT || 9090
+
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => {
+    console.log("Mongodb connected successfully")
+  })
+  .catch((err) => {
+    console.log("mongodb connection error ===> ", err)
+  })
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
