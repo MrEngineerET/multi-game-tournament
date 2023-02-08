0e7f7c8a30c786512f3185f42687d5f906f84838
Biruk Berhanu <meetbirukberhanu@gmail.com>
2023-02-08 13:46:33 +0300

    Add CORS support to Express server for cross-origin resource sharing

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,11 @@
 import express from "express"
 import tournamentRouter from "./routes/tournamentRoutes.js"
+import morgan from "morgan"
+import cors from "cors"
 
 const app = express()
+app.use(cors())
+
 app.use(express.static("public"))
 
 app.use(express.json())
@@ -10,11 +14,7 @@ app.use(
     extended: true,
   }),
 )
-
-app.get("/", (req, res) => {
-  res.send("Hello World!")
-})
-
-app.use("/tournament", tournamentRouter)
+app.use(morgan("tiny"))
+app.use("/api/v1/tournament", tournamentRouter)
 
 export { app }
