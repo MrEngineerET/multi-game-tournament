e271022ac21f2dab05d4139b9beb407b696d334b
Biruk Berhanu <meetbirukberhanu@gmail.com>
2023-02-24 18:04:08 +0300

    add delay to api requests on development environment

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,12 +2,52 @@ import pureAxios from "axios"
 import localStorage from "./localStorage"
 
 const token = localStorage.getItem("token")
-export const axios = pureAxios.create({
+
+const axioss = pureAxios.create({
   // eslint-disable-next-line no-undef
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     common: {
       "x-auth": token,
     },
   },
 })
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, ms)
+  })
+}
+
+export const getAxios = () => {
+  if (process.env.NODE_ENV === "development") {
+    return {
+      get: async (...rest) => {
+        await sleep(1000)
+        return axioss.get(...rest)
+      },
+      post: async (...rest) => {
+        await sleep(1000)
+        return axioss.post(...rest)
+      },
+      delete: async (...rest) => {
+        await sleep(1000)
+        return axioss.delete(...rest)
+      },
+      patch: async (...rest) => {
+        await sleep(1000)
+        return axioss.patch(...rest)
+      },
+      put: async (...rest) => {
+        await sleep(1000)
+        return axioss.put(...rest)
+      },
+    }
+  } else {
+    return axioss
+  }
+}
+
+export const axios = getAxios()
