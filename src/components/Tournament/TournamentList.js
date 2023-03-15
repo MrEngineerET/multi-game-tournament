2137b7ec916131c729130e01855027518123142d
Biruk Berhanu <meetbirukberhanu@gmail.com>
2023-03-15 15:00:33 +0300

    updates tourament list ui on tournament page

--- src/components/Tournament/TournamentList.js
+++ src/components/Tournament/TournamentList.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { Box, Typography, Card, Button, ButtonBase } from "@mui/material"
 import { Await, useLoaderData } from "react-router-dom"
@@ -22,6 +22,8 @@ import {
   Avatar,
   AvatarGroup,
 } from "@mui/material"
+import { Tabs, Tab } from "@mui/material"
+import { useTheme, useMediaQuery } from "@mui/material"
 import DeleteIcon from "@mui/icons-material/Delete"
 import ArchiveIcon from "@mui/icons-material/Archive"
 import DoneAllIcon from "@mui/icons-material/DoneAll"
@@ -34,7 +36,7 @@ export function TournamentList() {
     <Box sx={{ width: 1 }}>
       <Card
         sx={{ bgcolor: "background.lightest", p: 3, pt: 8, pb: 10 }}
-        elevation={3}
+        elevation={0}
       >
         <React.Suspense fallback={<TournamentSkeleton />}>
           <Await
@@ -77,43 +79,124 @@ export function TournamentList() {
   )
 }
 
+const sxStyles = {
+  tabs: { maxWidth: 500, margin: "auto", minHeight: 35 },
+  tab: (theme) => ({
+    "&.Mui-selected": {
+      bgcolor: "background.paper",
+    },
+    [theme.breakpoints.down("sm")]: {
+      padding: 0,
+      flex: 1,
+      fontSize: 12,
+      minHeight: 35,
+    },
+  }),
+  listItemButton: {
+    "&.Mui-selected": {
+      bgcolor: "background.lighter",
+    },
+    "&.Mui-selected:hover": {
+      bgcolor: "background.lighter",
+    },
+  },
+}
+
+const filteringOptions = {
+  all: "all",
+  pending: "pending",
+  progress: "progress",
+  completed: "completed",
+  archived: "archived",
+}
 export function TournamentFilter() {
+  const theme = useTheme()
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"))
+  const isMediumScreen = useMediaQuery(theme.breakpoints.down("md"))
+
+  const [selectedFilter, setSelectedFilter] = useState(filteringOptions.all)
+
+  const handleFilterChange = (option) => {
+    setSelectedFilter(option)
+  }
+
+  if (isMediumScreen)
+    return (
+      <Tabs
+        value={selectedFilter}
+        onChange={(e, newValue) => handleFilterChange(newValue)}
+        sx={sxStyles.tabs}
+        variant={isSmallScreen ? "scrollable" : "standard"}
+      >
+        <Tab sx={sxStyles.tab} label="All" value={filteringOptions.all} />
+        <Tab
+          sx={sxStyles.tab}
+          label="Pending"
+          value={filteringOptions.pending}
+        />
+        <Tab
+          sx={sxStyles.tab}
+          label="Progress"
+          value={filteringOptions.progress}
+        />
+        <Tab
+          sx={sxStyles.tab}
+          label="Completed"
+          value={filteringOptions.completed}
+        />
+        <Tab
+          sx={sxStyles.tab}
+          label="Arvhived"
+          value={filteringOptions.archived}
+        />
+      </Tabs>
+    )
+
   return (
-    <Card sx={{ bgcolor: "background.lightest" }}>
+    <Card sx={{ bgcolor: "background.lightest" }} elevation={0}>
       <List>
         <ListItemButton
-          selected
-          sx={{
-            "&.Mui-selected": {
-              bgcolor: "background.lighter",
-            },
-            "&.Mui-selected:hover": {
-              bgcolor: "background.lighter",
-            },
-          }}
+          selected={selectedFilter === filteringOptions.all}
+          sx={sxStyles.listItemButton}
+          onClick={() => handleFilterChange(filteringOptions.all)}
         >
           <ListItemText>All Tournaments</ListItemText>
         </ListItemButton>
-
-        <ListItemButton>
+        <ListItemButton
+          selected={selectedFilter === filteringOptions.pending}
+          sx={sxStyles.listItemButton}
+          onClick={() => handleFilterChange(filteringOptions.pending)}
+        >
           <ListItemIcon>
             <PendingOutlinedIcon />
           </ListItemIcon>
           <ListItemText>Pending</ListItemText>
         </ListItemButton>
-        <ListItemButton>
+        <ListItemButton
+          selected={selectedFilter === filteringOptions.progress}
+          sx={sxStyles.listItemButton}
+          onClick={() => handleFilterChange(filteringOptions.progress)}
+        >
           <ListItemIcon>
             <PlayCircleOutlinedIcon />
           </ListItemIcon>
           <ListItemText>In Progress</ListItemText>
         </ListItemButton>
-        <ListItemButton>
+        <ListItemButton
+          selected={selectedFilter === filteringOptions.completed}
+          sx={sxStyles.listItemButton}
+          onClick={() => handleFilterChange(filteringOptions.completed)}
+        >
           <ListItemIcon>
             <DoneAllIcon />
           </ListItemIcon>
           <ListItemText>Completed</ListItemText>
         </ListItemButton>
-        <ListItemButton>
+        <ListItemButton
+          selected={selectedFilter === filteringOptions.archived}
+          sx={sxStyles.listItemButton}
+          onClick={() => handleFilterChange(filteringOptions.archived)}
+        >
           <ListItemIcon>
             <ArchiveIcon />
           </ListItemIcon>
