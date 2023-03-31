cc3eb75a1b395c4a89d2edf5b6ef8c83e8f7fa85
Biruk Berhanu <meetbirukberhanu@gmail.com>
2023-03-31 08:27:07 +0300

    refactor TournamentStatus component

--- src/components/Tournament/TournamentStatus.js
+++ src/components/Tournament/TournamentStatus.js
@@ -5,50 +5,51 @@ import { useFetcher, useLocation } from "react-router-dom"
 
 export function TournamentStatus() {
   const { tournamentData } = useTournamentContext()
-  const tournamentStatus = tournamentData.status
   const stage = tournamentData.stages[0]
   const location = useLocation()
   const fetcher = useFetcher()
 
+  const isPending = tournamentData.status === "pending"
+
+  const isReadyToStart =
+    isPending && tournamentData.participants.length >= 2 && stage
+
+  const showAddParticipantInfo =
+    !location.pathname.includes("participants") &&
+    tournamentData.participants.length < 2
+
   return (
     <Box>
-      {tournamentStatus !== "progress" &&
-        tournamentData.participants.length >= 2 &&
-        stage && (
-          <>
-            <Alert severity="info" sx={{ mb: 5 }}>
-              <AlertTitle>Info</AlertTitle>
-              <Typography>
-                Once your bracket is looking good and participants are ready:
-              </Typography>
-              <fetcher.Form
-                method="post"
-                action={`/tournament/${tournamentData._id}?type=updateStatus`}
+      {isReadyToStart && (
+        <>
+          <Alert severity="info" sx={{ mb: 5 }}>
+            <AlertTitle>Info</AlertTitle>
+            <Typography sx={{ mb: 3 }}>
+              Once your bracket is looking good and participants are ready:
+            </Typography>
+            <fetcher.Form
+              method="post"
+              action={`/tournament/${tournamentData._id}?type=updateStatus`}
+            >
+              <input hidden value="progress" name="status" readOnly />
+              <Button
+                type="submit"
+                disabled={
+                  fetcher.state === "submitting" || fetcher.state === "loading"
+                }
               >
-                <input hidden value="progress" name="status" readOnly />
-                <Button
-                  type="submit"
-                  sx={{ mt: 5 }}
-                  disabled={
-                    fetcher.state === "submitting" ||
-                    fetcher.state === "loading"
-                  }
-                >
-                  Start the tournament
-                </Button>
-              </fetcher.Form>
-            </Alert>
-            <Alert severity="info" sx={{ mb: 5 }}>
-              This bracket is a preview and subject to change until the
-              tournament is started.
-            </Alert>
-          </>
-        )}
+                Start the tournament
+              </Button>
+            </fetcher.Form>
+          </Alert>
+        </>
+      )}
       {!location.pathname.includes("settings") &&
+        isPending &&
         tournamentData.games.length === 0 && (
-          <Box>
+          <Box sx={{ mb: 5 }}>
             <Alert severity="info">
-              <Typography sx={{ pb: 3 }}>
+              <Typography sx={{ mb: 3 }}>
                 If you want to add games to your tournament
               </Typography>
               <Button href="settings">Add Game</Button>
@@ -56,21 +57,29 @@ export function TournamentStatus() {
           </Box>
         )}
 
-      {!location.pathname.includes("participants") &&
-        tournamentData.participants.length < 2 && (
-          <Box sx={{ mt: 5 }}>
-            <Alert severity="info">
-              <Typography sx={{ pb: 3 }}>
-                To get started add participants
-              </Typography>
-              <Button href="participants">Add Participants</Button>
-            </Alert>
-            <Alert severity="info" sx={{ mt: 5 }}>
-              A bracket preview will be displayed once 2 or more participants
-              are registered.
-            </Alert>
-          </Box>
-        )}
+      {showAddParticipantInfo && (
+        <Box sx={{ mt: 5 }}>
+          <Alert severity="info">
+            <Typography sx={{ pb: 3 }}>
+              To get started add participants
+            </Typography>
+            <Button href="participants">Add Participants</Button>
+          </Alert>
+        </Box>
+      )}
+      {isReadyToStart ? (
+        <Alert severity="info" sx={{ mb: 5 }}>
+          This bracket is a preview and subject to change until the tournament
+          is started.
+        </Alert>
+      ) : showAddParticipantInfo ? (
+        <Alert severity="warning" sx={{ mt: 5 }}>
+          A bracket preview will be displayed once 2 or more participants are
+          registered.
+        </Alert>
+      ) : (
+        ""
+      )}
     </Box>
   )
 }
