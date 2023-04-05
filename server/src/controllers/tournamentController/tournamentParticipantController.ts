524dee62ea9b37bc86478065c3649c8336944914
Biruk Berhanu <meetbirukberhanu@gmail.com>
2023-04-05 14:40:24 +0300

    implement delete participant api endpoint

--- server/src/controllers/tournamentController/tournamentParticipantController.ts
+++ server/src/controllers/tournamentController/tournamentParticipantController.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express"
 import { BracketsManager, helpers } from "brackets-manager"
-import { InputStage } from "brackets-model"
+import { InputStage, Seeding } from "brackets-model"
 import { MyDB } from "../../utils/MyDB"
 import { Tournament } from "../../model/tournament"
 
@@ -97,7 +97,60 @@ async function deleteParticipant(
   next: NextFunction,
 ) {
   try {
-    res.send("delete participant")
+    //TODO: add a validator to this controller(participant should be array, no duplication of participants)
+    const { tournamentId, participantId } = req.params
+    if (!participantId || !tournamentId) {
+      throw {
+        statusCode: 400,
+        message: "Participant or tournament id is missing",
+      }
+    }
+    let myDB = await MyDB.build(Number(tournamentId))
+    let manager = new BracketsManager(myDB)
+    // get current participants list
+    const tournamentData = await manager.export()
+    const currentParticipants = tournamentData.participant
+    // add the new participants
+    const updatedParticipants = currentParticipants.filter(
+      (p) => p.id !== Number(participantId),
+    )
+    let updatedParticipantsNames: Seeding = updatedParticipants.map(
+      (p) => p.name,
+    )
+    updatedParticipantsNames = helpers.balanceByes(updatedParticipantsNames)
+    helpers.ensureNoDuplicates(updatedParticipants)
+
+    // delete the stage
+    await Tournament.updateOne(
+      { _id: tournamentId },
+      {
+        $unset: {
+          stage: "",
+          round: "",
+          group: "",
+          match: "",
+          match_game: " ",
+          participant: "",
+          participantGameMatrix: "",
+        },
+      },
+    )
+    // create a new stage
+    const stage = tournamentData.stage[0]
+    delete stage.settings.size
+    const inputStage: InputStage = {
+      tournamentId: Number(tournamentId),
+      name: stage.name,
+      type: stage.type,
+      seeding: updatedParticipantsNames,
+      settings: stage.settings,
+    }
+    // create a new Storage interface and Bracket manager since I used the Tournament model directly to update the tournament
+    myDB = await MyDB.build(Number(tournamentId))
+    manager = await new BracketsManager(myDB)
+    await manager.create(inputStage)
+    const updatedTournament = await Tournament.findById(tournamentId)
+    res.status(200).send(updatedTournament)
   } catch (error) {
     next(error)
   }
