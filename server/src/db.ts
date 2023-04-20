5bafef377a42e62c8b256339647cb414824e5276
Biruk Berhanu <meetbirukberhanu@gmail.com>
2023-04-20 12:00:18 +0300

    handle undefine value

--- server/src/db.ts
+++ server/src/db.ts
@@ -2,7 +2,14 @@ import mongoose from "mongoose"
 
 const PORT = process.env.PORT || 9090
 async function openMongooseConnection(DB_URI = null): Promise<void> {
-  await mongoose.connect(DB_URI || process.env.DB_URI)
+  const URI = process.env.DB_URI
+  if (DB_URI) {
+    await mongoose.connect(DB_URI)
+  } else if (URI) {
+    await mongoose.connect(URI)
+  } else {
+    throw new Error("No DB_URI found in process.env")
+  }
   console.log("Mongoose connection opened successfully")
 }
 
