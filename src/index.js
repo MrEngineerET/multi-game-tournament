c89a4428ac102d3c8943482407afea967ed1cc00
Biruk Berhanu <meetbirukberhanu@gmail.com>
2023-06-03 16:13:00 +0300

    configure tailwind css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { useState, createContext, useMemo } from "react"
 import ReactDOM from "react-dom/client"
+import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 import { ThemeProvider } from "@mui/material/styles"
