3c0685ba097a0ee925d8440ae5dc8d1b19b6649c
Biruk Berhanu <meetbirukberhanu@gmail.com>
2023-07-27 14:09:18 +0300

    show tournament progress using a progress component instead of text

--- src/components/TournamentPage/TournamentListTable.js
+++ src/components/TournamentPage/TournamentListTable.js
@@ -12,13 +12,20 @@ import {
   TableBody,
   Checkbox,
 } from "@mui/material"
+import { useMediaQuery } from "@mui/material"
 
-import { Avatar, AvatarGroup } from "@mui/material"
+import {
+  Avatar,
+  AvatarGroup,
+  LinearProgress,
+  CircularProgress,
+} from "@mui/material"
 import { TableToolBar } from "./TableToolBar"
 
 export function TournamentListTable({ tournaments, filter }) {
   const [selected, setSelected] = React.useState([])
   const [isAllSelected, setIsAllSelected] = React.useState(false)
+  const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down("sm"))
 
   const filteredTournaments = tournaments.filter((tournament) => {
     if (filter == "all" || filter === null || filter.trim() === "") return true
@@ -53,10 +60,17 @@ export function TournamentListTable({ tournaments, filter }) {
     navigation.state === "submitting" || navigation.state === "loading"
 
   const getTournamentStatus = (tournament) => {
-    if (tournament.status === "completed") return "Completed"
-    if (tournament.status === "progress") return "Progress"
-    if (tournament.status === "pending") return "Pending"
     if (tournament.status === "archived") return "Archived"
+    if (isSmallScreen)
+      return (
+        <CircularProgressWithLabel
+          value={tournament.progress}
+          color="secondary"
+        />
+      )
+    return (
+      <LinearProgressWithLabel value={tournament.progress} color="secondary" />
+    )
   }
   const getDate = (rawDate) => {
     if (!rawDate) return null
@@ -240,4 +254,56 @@ export function TournamentListTable({ tournaments, filter }) {
 TournamentListTable.propTypes = {
   tournaments: PropTypes.array,
   filter: PropTypes.string,
+}
+
+function LinearProgressWithLabel(props) {
+  return (
+    <Box sx={{ display: "flex", alignItems: "center" }}>
+      <Box sx={{ width: "100%", mr: 1 }}>
+        <LinearProgress variant="determinate" color="secondary" {...props} />
+      </Box>
+      <Box sx={{ minWidth: 35 }}>
+        <Typography variant="body2" color="text.secondary">{`${Math.round(
+          props.value,
+        )}%`}</Typography>
+      </Box>
+    </Box>
+  )
+}
+
+LinearProgressWithLabel.propTypes = {
+  value: PropTypes.number.isRequired,
+}
+
+function CircularProgressWithLabel(props) {
+  return (
+    <Box sx={{ position: "relative", display: "inline-flex" }}>
+      <CircularProgress variant="determinate" {...props} />
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: "absolute",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Typography variant="caption" component="div" color="text.secondary">
+          {`${Math.round(props.value)}%`}
+        </Typography>
+      </Box>
+    </Box>
+  )
+}
+
+CircularProgressWithLabel.propTypes = {
+  /**
+   * The value of the progress indicator for the determinate variant.
+   * Value between 0 and 100.
+   * @default 0
+   */
+  value: PropTypes.number.isRequired,
 }
